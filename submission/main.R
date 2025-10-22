ec497fb4a149ae6e066f350010705c1b1b323640
noahfehr <noah.fehr@yahoo.com>
2025-10-22 12:37:56 +0200

    First steps in cleaning the data, transforming the data, and standardizing the data

--- submission/main.R
+++ submission/main.R
@@ -5,7 +5,81 @@ library(dplyr)
 
 df <- utils::read.csv("data/data_quarterly.csv")
 head(df)
+# To build our model, we focus on data in the past 
+df_train <- df %>%
+  mutate(
+    date = as.Date(paste0(date, "-01"))
+  ) %>%
+  filter(date < as.Date("2025-10-01"))
+# Safety check to make sure all vals are numeric
+df_train <- df_train %>%
+  mutate(across(-date, as.numeric))
+# Check to confirm no missing values
+sapply(df_train, function(x) sum(is.na(x))) %>% sort(decreasing = TRUE)
 
+metadata <- utils::read.csv("data/metadata_quarterly_en.csv")
+# We need to transform the raw values to make the series stationary
+unit_to_transform <- c(
+  "real in millions of francs" = "logdiff",
+  "Index" = "logdiff",
+  "in millions of francs at current prices" = "logdiff",
+  "Gross domestic product (adjusted for sporting events) per hour worked" = "logdiff",
+  "Gross domestic product (adjusted for sporting events) per full-time equivalent" = "logdiff",
+  "in thousands of persons" = "diff",
+  "Price deflator" = "logdiff",
+  "in US dollars" = "logdiff",
+  "Real value of gross domestic product per full-time equivalent" = "logdiff",
+  "in percent" = "diff",
+  "real in millions of US dollars" = "logdiff",
+  "in thousands of Swiss francs at current prices" = "logdiff",
+  "real in thousands of Swiss francs" = "logdiff",
+  "rate" = "diff"
+)
+metadata$transform <- unit_to_transform[metadata$unit]
+# Now each column has its transformation stored with it in metadata
+
+#Helper function
+transform_variable <- function(x, transform_type) {
+  if (transform_type == "logdiff") {
+    return(c(NA, diff(log(x)) * 100))
+  } else if (transform_type == "diff") {
+    return(c(NA, diff(x)))
+  } else {
+    return(x)
+  }
+}
+
+# Apply transformations to the df_clean dataset
+
+df_train_clean <- df_train
+
+for (i in seq_len(nrow(metadata))) {
+  var <- metadata$variable[i]
+  t_type <- metadata$transform[i]
+  
+  # Skip if variable not in df_train
+  if (!var %in% names(df_train)) next
+  
+  df_train_clean[[var]] <- transform_variable(df_train[[var]], t_type)
+}
+# First row using diff makes na
+df_train_clean <- df_train_clean[-1, ]
+head(df_train_clean)
+
+# Now we need to standardize everything
+df_train_std <- df_train_clean
+
+df_train_std[-1] <- scale(df_train_clean[-1]) # scale subtracts col mean and divides by SD
+head(df_train_std)
+
+# Saving the means and sd just in case we need later
+scaler_params <- data.frame(
+  variable = names(df_train_clean)[-1],
+  mean = apply(df_train_clean[-1], 2, mean, na.rm = TRUE),
+  sd = apply(df_train_clean[-1], 2, sd, na.rm = TRUE)
+)
+
+## Below is just demo code
 # Create sample data
 data <- data.frame(
   x = rnorm(100),
